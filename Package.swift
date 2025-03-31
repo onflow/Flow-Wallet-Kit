7eaec20d6373b3ba6b86b528eb5f01b13f3dcad2
lmcmz <lmcmze@gmail.com>
2025-03-31 18:04:57 +1100

    Update flow-swift version

--- Package.swift
+++ Package.swift
@@ -16,9 +16,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", from: "4.2.2"),
-        .package(url: "https://github.com/outblock/flow-swift.git", from: "0.3.9"),
-        .package(url: "https://github.com/Outblock/wallet-core", .branchItem("master")),
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.50.4"),
+        .package(url: "https://github.com/outblock/flow-swift.git", from: "0.4.1"),
+        .package(url: "https://github.com/Outblock/wallet-core", .branchItem("master"))
     ],
     targets: [
         .target(
