ab0bcd72d1126c4b9085100e82043533e7988552
lmcmz <lmcmze@gmail.com>
2025-04-02 02:36:45 +1100

    Add WIP empty file

---
+++ iOS/FlowWalletKit/Sources/Backup/MultiBackup/MultiBackupProviderType.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  FlowWalletKit
+//
+//  Created by Hao Fu on 2/4/2025.
+//
+
+import Foundation
+
+// WIP
+protocol CloudKeyStorageProtocol {}
+
+enum MultiBackupProviderType {
+    case googleDrive
+    case icloud
+    case dropbox
+}
---
+++ iOS/FlowWalletKit/Sources/dApp/FCL/WCFCLHandler.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  FlowWalletKit
+//
+//  Created by Hao Fu on 2/4/2025.
+//
+
+import Foundation
