6c6d06e89afb9d039036a4f69210942f0fe34168
lmcmz <lmcmze@gmail.com>
2025-04-30 17:46:24 +1000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", from: "4.2.2"),
-        .package(url: "https://github.com/Outblock/flow-swift", .revisionItem("489177ca2f3fee133cf3c1ce7d6c0cc2892c34ef")),
+        .package(url: "https://github.com/Outblock/flow-swift", .revisionItem("82b3d2c72b1fd2f945b5bd16dcef6635351e062a")),
         .package(url: "https://github.com/trustwallet/wallet-core", .upToNextMajor(from: "4.3.2")),
     ],
     targets: [
